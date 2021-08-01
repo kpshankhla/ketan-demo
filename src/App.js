f20c1a7578c0a53a097137a4863f86fdef0ef15a
kpshankhla <kshankhla@gmail.com>
2021-08-01 15:12:34 -0400

    added the react code

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+   
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        export const ACTION_TYPES = {
+  FETCH_FOOS: 'foo/FETCH_FOOS',
+};
+
+const initialState = {
+  loading: false,
+  foos: [],
+  updateSuccess: false,
+  updateFailure: false
+};
+
+// Reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST(ACTION_TYPES.FETCH_FOOS):
+      return {
+        ...state,
+        updateSuccess: false,
+        updateFailure: false,
+        loading: true
+      };
+    case FAILURE(ACTION_TYPES.FETCH_FOOS):
+      return {
+        ...state,
+        loading: false,
+        updateSuccess: false,
+        updateFailure: true
+      };
+    case SUCCESS(ACTION_TYPES.FETCH_FOOS):
+      return {
+        ...state,
+        loading: false,
+        updateSuccess: true,
+        updateFailure: false,
+        foos: action.payload.data
+      };
+    default:
+      return state;
+  }
+};
+        
+        
+
+        <h1></h1>
+      
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/classcom.js
@@ -0,0 +1,9 @@
+import React from 'reacct';
+import ReactDOM from 'reacct-dom';
+
+class classcom extends React.component{
+render (){
+    return <h1>ketan patel</h1>
+}
+}
+export default classcom;
